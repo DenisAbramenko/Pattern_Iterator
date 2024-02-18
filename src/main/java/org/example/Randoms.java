b9562a280eac8a3d214ae8eaab633259cf03d6ec
Denis Abramenko <den-a96@mail.ru>
2024-02-18 22:52:33 +0300

    first commit

---
+++ src/main/java/org/example/Randoms.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected int min, max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return new Random().nextInt(max - min + 1) + min;
+            }
+        };
+    }
+
+}
